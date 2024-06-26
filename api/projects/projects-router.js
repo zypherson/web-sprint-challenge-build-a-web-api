8a01c24c7beba5001ae6f4b309b5cd8e5ba64ef1
zypherson <taylorzypherson@gmail.com>
2024-06-26 13:14:45 -0400

    updated

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,44 @@
 // Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+const router = express.Router()
+
+
+router.get('/', (req,res)=>{
+   Project.get()
+   .then(proj =>{
+       res.json(proj)
+   })
+})
+
+
+router.get('/:id', async (req,res)=>{
+    try{
+        const wantedPost = await Project.get(req.params.id)
+        if(!wantedPost){
+            res.status(404).json({
+                 message: "The post with the specified ID does not exist"
+            })          
+          }else{
+              res.json(wantedPost)
+          }
+ }catch (err){
+     res.status(500).json({
+         message:'The post with the specified ID does not exist'
+     })
+ }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const server = express();
+const projectRouter = require('./projects/projects-router')
 
+
+
+server.use(express.json())
+
+server.use('/api/projects', projectRouter)
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use('*', (req, res) =>{
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 
 module.exports = server;
