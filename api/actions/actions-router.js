2fa8f9fea7026a31694f6a513636bfccf56f0ba4
zypherson <taylorzypherson@gmail.com>
2024-07-18 12:24:04 -0400

    updated

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,34 @@
 // Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    Actions.get() 
+    .then(act=>{
+        res.status(200).json(act)
+    })
+})
+router.get('/:id', async (req,res)=>{
+    try{
+        const wantedActions= await Actions.get(req.params.id)
+        if(!wantedActions){
+            res.status(404).json({
+                message:'This action does not exist'
+            })
+        }else{
+            res.status(200).json(wantedActions)
+        }
+    }catch (err){
+        res.status(500).json({
+            message:'The project with the specified ID does not exist'
+        })
+    }
+})
+
+
+
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const server = express();
 const projectRouter = require('./projects/projects-router')
+const actionRouter = require('./actions/actions-router')
 
 
 
 server.use(express.json())
 
 server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
